6495bce1c4143a708690778098a6046cda331abf
LingXiFox <handsomezhu2019@outlook.com>
2025-09-27 23:30:36 +0800

    Initial Commit

---
+++ Tools/ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolsApp.swift
+//  Tools
+//
+//  Created by LingXiFox on 2025/9/27.
+//
+
+import SwiftUI
+
+@main
+struct ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToolsTests/ToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToolsTests.swift
+//  ToolsTests
+//
+//  Created by LingXiFox on 2025/9/27.
+//
+
+import Testing
+@testable import Tools
+
+struct ToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
